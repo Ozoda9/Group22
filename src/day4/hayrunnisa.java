3c0bad015234241fa0ac5c5e02fcdb5d12764728
Veronica <marandici.veronica@gmail.com>
2022-08-31 12:20:23 -0400

    Veronica update

--- src/day4/hayrunnisa.java
+++ src/day4/hayrunnisa.java
@@ -1,7 +1,9 @@
 package day4;
 
 public class hayrunnisa{
+    public static void main(String[] args) {
 
-    System.out.println(" Hello Classmate");
 
+        System.out.println(" Hello Classmate");
+    }
 }
